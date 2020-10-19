374ffc2f3ffb839007f60b7d6b3046e5dacd980c
王连喜 <wanglianxi@bmsoft.com.cn>
2020-10-19 14:54:29 +0800

    redis实战 使用redis构建文章排行榜应用（简易）

--- src/test/java/com/xizi/redis_action/redis/string/VoteTest.java
+++ src/test/java/com/xizi/redis_action/redis/string/VoteTest.java
@@ -6,14 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisZSetCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -39,7 +38,13 @@ public class VoteTest {
     public static String getFullKey(String prefix,Article article){
         return String.format(prefix,article.getArticleId());
     }
+    public static String getFullKey(String prefix,String id){
+        return String.format(prefix,id);
+    }
     
+    /** 发送文章
+     * @param article
+     */
     public void pushArticle(Article article){
         redisTemplate.opsForSet().add(getFullKey(VOTED_PREFIX,article),article.getAuthor());
         redisTemplate.expire(getFullKey(VOTED_PREFIX,article),Duration.ofSeconds(WEEK_TO_SECOND));
@@ -57,6 +62,10 @@ public class VoteTest {
     
     }
     
+    /** 点赞文章
+     * @param user
+     * @param article
+     */
     public void vote(User user,Article article){
         long offsetTime = (System.currentTimeMillis() / 1000) - WEEK_TO_SECOND;
         if(redisTemplate.opsForZSet().score("time",article.getArticleId()) < offsetTime){
@@ -68,10 +77,57 @@ public class VoteTest {
         }
     }
     
+    /** 分页获取文章
+     * @param start
+     * @param end
+     * @return
+     */
+    public List<Map> getArticles(long start,long end){
+        Set<String> set = redisTemplate.opsForZSet().reverseRange("score",start,end);
+        List<Map> list = new ArrayList();
+        set.forEach(e -> {
+            Map<Object, Object> map = redisTemplate.opsForHash().entries(getFullKey(ARTICLE_PREFIX,e));
+            list.add(map);
+        });
+        return list;
+    }
+    
+    /** 将文章放入群组
+     * @param group
+     * @param articleId
+     */
+    public void addGroup(String group,String articleId){
+        redisTemplate.opsForSet().add(group,articleId);
+    }
+    
+    /** 获取某个组中的排行榜
+     * @param group
+     * @return
+     */
+    public Set<String> getGroupArticles(String group,long end){
+        redisTemplate.opsForZSet().intersectAndStore(group,Arrays.asList("score"),group + "score", RedisZSetCommands.Aggregate.MAX);
+        return redisTemplate.opsForZSet().reverseRange(group + "score",0,end);
+    }
+    
     @Test
     public void test(){
-        Article article = new Article("wz1","zz1");
-        pushArticle(article);
+//        Article article = new Article("wz1","zz1");
+        Article article2 = new Article("wz2","zz2");
+        Article article3 = new Article("wz3","zz3");
+        Article article4 = new Article("wz4","zz4");
+        Article article5 = new Article("wz5","zz5");
+//        pushArticle(article);
+        pushArticle(article2);
+        pushArticle(article3);
+        pushArticle(article4);
+        pushArticle(article5);
+        addGroup("18x","wz2");
+        addGroup("18x","wz3");
+        addGroup("18x","wz1");
+        addGroup("18x","wz4");
+        addGroup("18x","wz5");
+    
+        System.out.println(getGroupArticles("18x",2));
     }
     
     @Test
